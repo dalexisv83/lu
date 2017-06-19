716f9513ab2afa2fab9b1049b4366694cd453ae8
Velasquez, David (dv365y) <dv365y@att.com>
2017-06-19 16:32:10 -0700

    Implements search input filtering by category

--- controllers/GiftCardCtrl.js
+++ controllers/GiftCardCtrl.js
@@ -20,6 +20,40 @@
                     DTColumnBuilder.newColumn('OfferType').withTitle('Offer Type'),
                     DTColumnBuilder.newColumn('RedemptionType').withTitle('Redemption Type')
                 ];
+
+                $scope.giftCardInst = {};
+
+                //$http.jsonp(URLS.API_DEV + 'web/api/datalookup/GiftCardCat?callback=JSON_CALLBACK').then(function successCallback(response) {
+                //    $scope.categories = response.data;
+                //}, function errorCallback(response) {
+                //    throw new Error('Data request failed:\n' + JSON.stringify(response));
+                //});
+
+                $scope.categories = [{
+                    "category": "did",
+                    "label": "Dealer ID"
+                }, {
+                    "category": "ya-offer",
+                    "label": "YA Offer #"
+                }, {
+                    "category": "b-code",
+                    "label": "B-Code"
+                }, {
+                    "category": "offer-code",
+                    "label": "Offer Code"
+                }, {
+                    "category": "dnis",
+                    "label": "DNIS"
+                }, {
+                    "category": "",
+                    "label": "All offer types"
+                }];
+
+                $scope.$watch('category', function () {
+                    if ($scope.giftCardInst.DataTable) {
+                        $scope.giftCardInst.DataTable.column(3).search($scope.category).draw();
+                    }
+                });
             }
         ]);
 }(window.angular));
